6ee76a3cf6acc1b3259d59174316d939aaa7513d
Meseret <meseretmaru.wassie@gmail.com>
2023-02-21 11:01:05 -0800

    felegehiwot hospital

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *emm
+ *
+ * That may: mister menilik(emperor)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
