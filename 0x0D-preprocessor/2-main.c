02b549c1ef6fa59e95ac00c31aa5540a28eb48d6
Meseret <meseretmaru.wassie@gmail.com>
2023-03-20 19:21:55 -0700

    j

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ * Return: 0
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
